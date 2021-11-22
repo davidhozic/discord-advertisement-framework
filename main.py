66300697ca17275dfdb11b5e9555a84afb35ee63
David Hozic <davidhozic@gmail.com>
2021-11-22 06:27:53 +0100

    Block fix

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+from asyncio.tasks import gather
 import discord, time, asyncio, random
 from discord.ext import commands
 
 
 # CONSTANTS
-C_BOT_API_KEY = "OTA5MDYwMTIwNjIzODQxMzAy.YZVSvA.2QDp_iJcVXPQ_9LqUsvI4ZPwWmw"
+C_BOT_API_KEY = "OTA5MDYwMTIwNjIzODQxMzAy.YZVSvA.2QDp_iJcVXPQ_9LqUsvI4ZPwWmw"#"OTExNzQwOTk4NzUxNzcyNzEy.YZl0qg.mmcNWF__27YgtMEnJ6X95oYbG2s"
 C_DEBUG = True
 C_IS_USER = True
-C_LOG_FILE_NAME = "log.txt"
-C_MESSAGE = '😈 🅰️🇩🅰️  🇺 🇳 🇩 🇪 🇷 🇼 🇴 🇷 🇱 🇩  😈 \n\n               - a new upcoming project!\n\n                 {INSERT YOUR INVITE LINK HERE}\n\n                     ONLY 500 WHITELIST SPOTS\n\n      ADA Underworld is releasing their Set 1 - Devils!\n\nThe release of 750 unique, hand-drawn, randomly generated\n    NFTs on the Cardano blockchain coming early 2022!\n\nCome join the server for many giveaways, whitelist,\n                 awsome community and much more!\n\n\nCome check it out!\n\nJoin our discord!\nCheck our Twitter: https://twitter.com/ada_underworld\nhttps://cdn.discordapp.com/attachments/898701888676069386/910593996949168158/Underworld_shill_gif.gif'
+C_MESSAGE = """😈 🅰️🇩🅰️  🇺 🇳 🇩 🇪 🇷 🇼 🇴 🇷 🇱 🇩  😈 
+
+               - a new upcoming project!
+
+                 https://discord.gg/kRNnZtsP37
+
+                     **ONLY 500 WHITELIST SPOTS**
+
+      ADA Underworld is releasing their Set 1 - **Devils**!
+
+The release of 750 unique, hand-drawn, randomly generated
+    NFTs on the Cardano blockchain coming early 2022!
+
+Come join the server for many **giveaways, whitelist**,
+                 awsome community and much more!
+
+
+Come check it out!
+
+Join our discord!
+Check our Twitter: https://twitter.com/ada_underworld
+https://cdn.discordapp.com/attachments/898701888676069386/910593996949168158/Underworld_shill_gif.gif"""
 
 
 ## Hour constants
 C_HOUR_TO_SECOND= 3600
 C_MINUTE_TO_SECOND = 60
-
-# Globals
-m_bot = discord.Client()#commands.Bot("::", description="Commands")
-
 
 
 # Classes
@@ -57,33 +74,37 @@ class MESSAGE:
 
 class GUILD:
     server_list = []
+    bot_object = discord.Client()
 
     def __init__(this, guildid, channels_to_send_in, messages_to_send):
         this.guild =    guildid
         this.messages = messages_to_send
         this.channels = channels_to_send_in
-        this.guild_name = None
+        this.guild_file_name = None
+        GUILD.server_list.append(this)
     def initialize(this):       # Get objects from ids
-        this.guild = m_bot.get_guild(this.guild)
-        this.channels = [this.guild.get_channel(x) for x in this.channels]
-        this.guild_name = this.guild.name
+        this.guild = GUILD.bot_object.get_guild(this.guild)
+        if this.guild != None:
+            this.channels = [this.guild.get_channel(x) for x in this.channels]
+            this.guild_file_name = this.guild.name.replace("/","_").replace("\\","_").replace(":","_").replace("!","_").replace("|","_").replace("*","_").replace("?","_").replace("<","_").replace(">", "_") + ".txt"
 
     async def advertise(this, force=False):
         l_trace = ""
-        for l_msg in this.messages:
-            if force or (l_msg.timer.start() and l_msg.timer.elapsed() > l_msg.period):
-                if l_msg.randomized_time == True:           # If first parameter to msg object is != 0
-                        l_msg.period = random.randrange(*l_msg.random_range)
-                l_msg.timer.reset()
-                l_msg.timer.start()
-                l_msg.generate_timestamp()
-                l_trace += f'Sending Message: "{l_msg.text}"\n\nServer: {this.guild_name}\nChannels: {[x.name for x in this.channels]} \nTimestamp: {l_msg.last_timestamp}\n\n----------------------------------\n\n'
-                TRACE(l_trace)
-                for l_channel in this.channels:
-                    await l_channel.send(l_msg.text)
-                    time.sleep(4)
-                    
-        return l_trace
+        if this.guild != None:
+            for l_msg in this.messages:
+                if force or (l_msg.timer.start() and l_msg.timer.elapsed() > l_msg.period):
+                    if l_msg.randomized_time == True:           # If first parameter to msg object is != 0
+                            l_msg.period = random.randrange(*l_msg.random_range)
+                    l_msg.timer.reset()
+                    l_msg.timer.start()
+                    l_msg.generate_timestamp()
+                    l_trace += f'Sending Message: "{l_msg.text}"\n\nServer: {this.guild.name}\nChannels: {[x.name for x in this.channels]} \nTimestamp: {l_msg.last_timestamp}\n\n----------------------------------\n\n'
+                    TRACE(l_trace)
+                    for l_channel in this.channels:
+                        asyncio.gather(asyncio.create_task(l_channel.send(l_msg.text)))
+                        await asyncio.sleep(2)
+
+        return l_trace if l_trace != "" else None
         
 
 
@@ -92,20 +113,21 @@ class GUILD:
 ############################################################################################
 
 
-server_Solnanas = GUILD(   # ID Serverja
-                        890131961992081439,   
-                        # Tabela Kanalov                       
-                        [   
-                            893592192147398666            
-                        ],
-                        # Tabela sporocil
-                        [   #       minimum-sec                     maximum-sec           sporocilo
-                            MESSAGE(360 * C_MINUTE_TO_SECOND, 420 * C_MINUTE_TO_SECOND , C_MESSAGE)
-                            # MESSAGE(0, 10*C_MINUTE_TO_SECOND, "TEST")  # Ce je prvi parameter 0, potem je cas vedno drugi parameter drugace pa sta prva dva parametra meje za nakljucno izbiro
-                        ]
-)
+server_ = GUILD(
+        0,                          # ID Serverja
+        [                                            # Tabela ID Channels
+            0            
+        ],
+        # Messages
+        [   #       min-sec                     max-sec           sporocilo
+            MESSAGE(0*C_HOUR_TO_SECOND, 0*C_HOUR_TO_SECOND , C_MESSAGE)
+            # MESSAGE(0, 10*C_MINUTE_TO_SECOND, "TEST")  # Ce je prvi parameter 0, potem je cas vedno drugi parameter drugace pa sta prva dva parametra meje za nakljucno izbiro
+        ]
+    )
 
-GUILD.server_list = [server_Solnanas]
+
+
+                                     
 ############################################################################################
 
 # Debugging functions
@@ -114,9 +136,9 @@ def TRACE(message):
         print(message)
 
 # Event functions
-@m_bot.event
+@GUILD.bot_object.event
 async def on_ready():
-    TRACE(f"Logged in as {m_bot.user}")
+    TRACE(f"Logged in as {GUILD.bot_object.user}")
     l_advertiser = asyncio.create_task(advertiser())
     asyncio.gather(l_advertiser)
 
@@ -126,18 +148,18 @@ async def advertiser():
         l_server.initialize()
         l_ret = await l_server.advertise(True)
         if l_ret != None:
-            with open(f"{l_server.guild_name}_{C_LOG_FILE_NAME}",'a', encoding='utf-8') as l_logfile:
+            with open(f"{l_server.guild_file_name}",'a', encoding='utf-8') as l_logfile:
                 l_logfile.write(l_ret)
     while True:
         await asyncio.sleep(1)
         for l_server in GUILD.server_list:
             l_ret = await l_server.advertise(False)
             if l_ret != None:
-                with open(f"{l_server.guild_name}_{C_LOG_FILE_NAME}",'a', encoding='utf-8') as l_logfile:
+                with open(f"{l_server.guild_file_name}",'a', encoding='utf-8') as l_logfile:
                     l_logfile.write(l_ret)
 
 def main():
-    m_bot.run(C_BOT_API_KEY, bot=not C_IS_USER)
+    GUILD.bot_object.run(C_BOT_API_KEY, bot=not C_IS_USER)
 
 if __name__ == "__main__":
     main()
