49731ed13dce77ecf1294ac23d930242d65b5a94
David Hozic <davidhozic@gmail.com>
2021-12-04 19:32:18 +0100

    debug update

--- Config.py
+++ Config.py
@@ -1,8 +1,13 @@
+
 # CONSTANTS
-C_BOT_API_KEY = "OTA5MzgyNDE3MDg3ODgxMjc2.YZDeXw.2oH3R1ESb_8oVGdUFnfm_wZc4Oo"
-C_DEBUG = False
-C_FILE_OUTPUT = True
+C_BOT_API_KEY = "OTA5MzgyNDE3MDg3ODgxMjc2.YZDeXw.2oH3R1ESb_8oVGdUFnfm_wZc4Oo"   
 C_IS_USER = False
+
+
+C_DEBUG = True                  # Will Print to console
+C_DEBUG_FILE_OUTPUT = False     # If C_DEBUG is also true, will print into file
+
+C_SERVER_FILE_LOG = True        # Make a file log for sent messages
 
 ## Hour constants
 C_DAY_TO_SECOND = 86400
