e70e6e68761aac1ffa6f6d6d0a49432a75e0a71d
David Hozic <davidhozic@gmail.com>
2021-12-12 12:12:22 +0100

    Cleanup

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
-import asyncio
 import  framework, Config
 from framework import GUILD, MESSAGE
 
-import FE_UNI.arduino
-import FE_UNI.uni
-    
 
 ############################################################################################
 #                               GUILD MESSAGES DEFINITION                                  #
 ############################################################################################
 
 GUILD.server_list = [
-GUILD(
-        863071397207212052,                          # ID Serverja
-        # Messages
-        [   #       min-sec                     max-sec      sporocilo   #IDji kanalov
-            MESSAGE(start_period=0, end_period=15 , text=FE_UNI.uni.get_msg, channels=[863071397207212056], clear_previous=False, start_now=True),
-            MESSAGE(start_period=0, end_period=1*Config.C_DAY_TO_SECOND , text=FE_UNI.arduino.get_msg, channels=[863071397207212056], clear_previous=True, start_now=True)
-        ]
-    )
 ]
                                      
 ############################################################################################
