885a9b4ca7eda4d5de1144937025f9b29c487fdb
David Hozic <davidhozic@gmail.com>
2022-03-13 13:27:01 +0100

    Split into multiple files (#51)
    * Increment version: 1.7.8 -> 1.7.9
    * Split into multiple files

--- src/framework/__init__.py
+++ src/framework/__init__.py
@@ -2,6 +2,13 @@
     DISCORD ADVERTISEMENT FRAMEWORK (DAF)
     Author      :: David Hozic
     Copyright   :: Copyright (c) 2022 David Hozic
-    Version     :: V1.7.8
+    Version     :: V1.7.9
 """
-from .__core__ import *
+import _discord as discord
+from .client import *
+from .const import *
+from .core import *
+from .dtypes import *
+from .guild import *
+from .message import *
+from .tracing import *
