2eb65c4259cc14837f9aa778319b2662ceadafe8
David Hozic <davidhozic@gmail.com>
2022-06-07 12:00:25 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("./requirements.txt" , 'r', encoding="utf-8") as rf:
 
 __metadata__ = \
 {
-    "version" :  "1.9.rc2",
+    "version" :  "1.9",
     "requirements" : req,
     "minimum_py_version" :  "3.8",
 }
