39f188a2ac93ad295cce8611bb66d747b8e8c774
David Hozic <davidhozic@gmail.com>
2023-02-07 14:02:41 +0100

    installl

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 # Installs required build packages
-
+echo "Creating Python  environment"
 python3 -m pip install virtualenv
 python3 -m virtualenv venv
 .\venv\Scripts\activate
@@ -8,9 +8,14 @@ python3 -m pip install --editable .[all]
 python3 -m pip install --editable .[docs]
 python3 -m pip install --editable .[testing]
 
+echo "Installing chocolatey, inkscape, mingw"
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install inkscape -y
 choco install mingw -y
 
-wget -Uri https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe -OutFile 'Docker Desktop Installer.exe'
+$ProgressPreference = 'SilentlyContinue' # Way faster for some reason
+echo "Downloading Docker"
+Invoke-WebRequest -Uri https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe -OutFile 'Docker Desktop Installer.exe'
+
+echo "Installing docker (for texlive)"
 Start-Process 'Docker Desktop Installer.exe' -Wait install
