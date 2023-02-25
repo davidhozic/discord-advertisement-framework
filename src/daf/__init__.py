4aebd297fba4ec5cf82be7f112094db4a0eaa813
David Hozic <davidhozic@gmail.com>
2023-02-25 23:01:19 +0100

    Fixed versuin

--- src/daf/__init__.py
+++ src/daf/__init__.py
@@ -14,16 +14,5 @@ from .misc import DOCUMENTATION_MODE
 if DOCUMENTATION_MODE:
     from .misc import *
 
-import os
-# Parse version
-gh_release = os.environ.get("GITHUB_REF_NAME", default=None) # Workflow run release
-readthedocs_release = os.environ.get("READTHEDOCS_VERSION", default=None) # Readthe docs version
 
-VERSION = "v0.0.1"
-if gh_release is not None:
-    VERSION = gh_release
-elif readthedocs_release is not None:
-    VERSION = readthedocs_release
-
-VERSION = VERSION.removeprefix("v")
-del os
+VERSION = "v2.5.1"
