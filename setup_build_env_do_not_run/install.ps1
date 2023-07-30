6b6a0c5ac2fd111063f23bd6de96a8f21638a9c6
David Hozic <davidhozic@gmail.com>
2023-07-31 00:16:51 +0200

    fix install.ps1

--- setup_build_env_do_not_run/install.ps1
+++ setup_build_env_do_not_run/install.ps1
@@ -2,7 +2,7 @@
 echo "Creating Python  environment"
 python3 -m pip install virtualenv
 python3 -m virtualenv .venv
-.\venv\Scripts\activate
+.\.venv\Scripts\activate
 cp .\.venv\Scripts\python.exe .\.venv\Scripts\python3.exe
 cp .\.venv\Scripts\pythonw.exe .\.venv\Scripts\python3w.exe
 python3 -m pip install --editable .[all]
@@ -14,7 +14,7 @@ echo "Installing packages"
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex
 scoop bucket add extras
-scoop install mingw ffmpeg inkscape
+scoop install ffmpeg inkscape
 
 # Install TexLive inside WSL
 wsl sudo apt install -y texlive-latex-base texlive-latex-extra texlive-lang-european latexmk
