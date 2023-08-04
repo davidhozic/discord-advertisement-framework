7bac7c4032e4dd1f12b33ba69f462ae903061ba7
David Hozic <davidhozic@gmail.com>
2023-08-04 21:50:43 +0200

    fix

--- src/daf/message/voice_based.py
+++ src/daf/message/voice_based.py
@@ -340,7 +340,7 @@ class VoiceMESSAGE(BaseChannelMessage):
             # TODO: When fixed, replace with audio.stream.
             raw_data = audio.data
             filename = Path.home().joinpath(f"daf/tmp_{id(raw_data)}")
-            filename.touch()
+            filename.parent.mkdir(exist_ok=True, parents=True)
 
             with open(filename, "wb") as tmp_file:
                 tmp_file.write(raw_data)
